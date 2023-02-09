78bf8dd3c3dda0dad259fd9ce39e340fe4474778
houcem <hedhoucem@gmail.com>
2023-02-09 10:02:54 +0100

    display product image

--- 02-exercice-iphone13-app/js/main.js
+++ 02-exercice-iphone13-app/js/main.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
       product: "iPhone 13 Pro",
       description:
         "The iPhone 13 Pro is the most powerful iPhone ever. It features a 6.1-inch Super Retina XDR display, a 12MP triple camera system, and a 12MP TrueDepth front camera.",
+      image: "./assets/images/iphone-13-blue.png",
     };
   },
 });
