4dc8cd3e31beefc2f58299c522e6ce9fd023b62a
houcem <hedhoucem@gmail.com>
2023-02-09 10:25:03 +0100

    list of specs

--- 02-exercice-iphone13-app/js/main.js
+++ 02-exercice-iphone13-app/js/main.js
@@ -6,6 +6,14 @@ const app = Vue.createApp({
         "The iPhone 13 Pro is the most powerful iPhone ever. It features a 6.1-inch Super Retina XDR display, a 12MP triple camera system, and a 12MP TrueDepth front camera.",
       image: "./assets/images/iphone-13-blue.png",
       quantity: 3,
+      specs: [
+        "5G speed",
+        "A14 Bionic chip",
+        "12MP triple camera system",
+        "12MP TrueDepth front camera",
+        "6.1-inch Super Retina XDR display",
+        "IP68 water resistance",
+      ],
     };
   },
 });
